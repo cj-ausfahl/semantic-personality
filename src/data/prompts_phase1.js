085d2d52fbad6b31e96bb736188ae6a2db046eb8
CJ Ausfahl <cjausfahl221@gmail.com>
2026-02-03 13:39:01 -0500

    Update prompts_phase1.js

--- src/data/prompts_phase1.js
+++ src/data/prompts_phase1.js
@@ -1,8 +1,14 @@
-export const phase1 = [
-  "Weight",
-  "Distance",
-  "Silence",
-  "Edge",
-  "Order",
-  "Delay"
+export const phase1Sections = [
+  {
+    id: "spatial",
+    prompts: ["Weight", "Distance", "Edge", "Boundary"]
+  },
+  {
+    id: "temporal",
+    prompts: ["Delay", "Waiting", "Ending", "Beginning"]
+  },
+  {
+    id: "control",
+    prompts: ["Order", "Rule", "Authority", "Error"]
+  }
 ];

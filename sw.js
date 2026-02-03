097fb43776278a6d6b0ddf45b29c35f2fb83d45a
CJ Ausfahl <cjausfahl221@gmail.com>
2026-02-03 13:59:33 -0500

    Caching Changes

--- sw.js
+++ sw.js
@@ -1,16 +1,4 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("spa-cache").then(cache =>
-      cache.addAll([
-        "/",
-        "/index.html"
-      ])
-    )
-  );
-});
+// Service worker temporarily disabled during development
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("fetch", () => {});
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
-});
